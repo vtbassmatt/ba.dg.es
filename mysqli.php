d0051d62956c49ce23a76ab1aece0b2a37648c76
Matt Cooper <vtbassmatt@gmail.com>
2011-06-26 19:11:10 -0700

    Removed SQL creds

--- mysqli.php
+++ mysqli.php
@@ -4,9 +4,7 @@ define("SITE_FOURSQUARE", 1);
 
 define("MYSQL_HOST", "mysql.mattandchristy.net");
 define("MYSQL_DB",   "badges_test");
-define("MYSQL_USER", "badges_tester");
-define("MYSQL_PASS", "b4dg3s!!");
-
+require_once('mysql_creds.inc');
 
 $mysqli = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
 if(mysqli_connect_errno()) {
